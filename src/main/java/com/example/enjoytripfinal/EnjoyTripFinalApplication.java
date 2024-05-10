2d0cdc393943d840a21a723c0de2e37df229fc8e
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-05-10 14:26:26 +0900

    feat : init setting

---
+++ src/main/java/com/example/enjoytripfinal/EnjoyTripFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.enjoytripfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyTripFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnjoyTripFinalApplication.class, args);
+    }
+
+}
