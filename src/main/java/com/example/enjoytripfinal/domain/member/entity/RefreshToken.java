ff49b8b12f9a02d52c5305ba88533a2eddaabeb6
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-05-12 21:12:50 +0900

    [FEAT] Member / RefreshToken Entity 추가

---
+++ src/main/java/com/example/enjoytripfinal/domain/member/entity/RefreshToken.java
@@ -0,0 +1,29 @@
+package com.example.enjoytripfinal.domain.member.entity;
+
+import com.github.f4b6a3.ulid.UlidCreator;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshToken {
+    @Id
+    @Column(name = "id", columnDefinition = "BINARY(16)")
+    private UUID id = UlidCreator.getMonotonicUlid().toUuid();
+
+    private String tokenKey;
+
+    private String tokenValue;
+
+    public RefreshToken(String tokenKey,String tokenValue){
+        this.tokenKey = tokenKey;
+        this.tokenValue = tokenValue;
+    }
+}
