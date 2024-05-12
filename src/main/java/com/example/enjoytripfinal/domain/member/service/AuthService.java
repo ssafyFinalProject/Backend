dcc94ed7eba663fb9fd8a6f5f1d0b29629e4cb9a
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-05-12 22:00:05 +0900

    [FEAT] AuthService에 TokenProvider 의존성 추가

--- src/main/java/com/example/enjoytripfinal/domain/member/service/AuthService.java
+++ src/main/java/com/example/enjoytripfinal/domain/member/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.example.enjoytripfinal.domain.member.service;
 
+import com.example.enjoytripfinal.config.security.jwt.TokenProvider;
 import com.example.enjoytripfinal.domain.member.repository.MemberRepository;
 import com.example.enjoytripfinal.domain.member.repository.RefreshTokenRepository;
 import org.springframework.stereotype.Service;
@@ -7,12 +8,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthService {
     private final MemberRepository memberRepository;
-
     private final RefreshTokenRepository refreshTokenRepository;
 
-    public AuthService(MemberRepository memberRepository,RefreshTokenRepository refreshTokenRepository) {
+    private final TokenProvider tokenProvider;
+
+    public AuthService(
+            MemberRepository memberRepository,
+            RefreshTokenRepository refreshTokenRepository,
+            TokenProvider tokenProvider) {
         this.memberRepository = memberRepository;
         this.refreshTokenRepository = refreshTokenRepository;
+        this.tokenProvider = tokenProvider;
     }
 
     public void signUp() {
