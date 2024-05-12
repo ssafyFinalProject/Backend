e67c61621a9970a93df2d1fcf71fa758d0114b15
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-05-12 21:39:19 +0900

    [FEAT] Auth / Member Controller Init Setting

--- src/main/java/com/example/enjoytripfinal/domain/member/controller/AuthController.java
+++ src/main/java/com/example/enjoytripfinal/domain/member/controller/AuthController.java
@@ -1,10 +1,32 @@
 package com.example.enjoytripfinal.domain.member.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.enjoytripfinal.domain.member.dto.request.LoginRequestDto;
+import com.example.enjoytripfinal.domain.member.dto.request.SignUpRequestDto;
+import com.example.enjoytripfinal.domain.member.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signUp(@RequestBody SignUpRequestDto request) {
+        // 회원 가입
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody LoginRequestDto request) {
+        // 로그인
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(){
+        // 로그아웃
+    }
 }
--- src/main/java/com/example/enjoytripfinal/domain/member/controller/MemberController.java
+++ src/main/java/com/example/enjoytripfinal/domain/member/controller/MemberController.java
@@ -1,8 +1,8 @@
 package com.example.enjoytripfinal.domain.member.controller;
 
 import com.example.enjoytripfinal.domain.member.service.MemberService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -11,5 +11,25 @@ public class MemberController {
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> getMemberByNickname(@RequestParam(name = "nickname") String nickname){
+        // nickname으로 회원 정보 반환
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<Void> getMemberByJwt(){
+        // 자기 자신의 정보 반환
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateMember() {
+        // 회원 정보 수정
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteMember() {
+        // 회원 정보 삭제
     }
 }
