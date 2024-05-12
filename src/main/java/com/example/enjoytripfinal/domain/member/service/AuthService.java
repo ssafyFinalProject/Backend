7db8e610c7e8e18cd70bc884a5652e5cb3df8893
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-05-12 21:29:17 +0900

    [FEAT] Spring Security 적용

--- src/main/java/com/example/enjoytripfinal/domain/member/service/AuthService.java
+++ src/main/java/com/example/enjoytripfinal/domain/member/service/AuthService.java
@@ -1,14 +1,18 @@
 package com.example.enjoytripfinal.domain.member.service;
 
 import com.example.enjoytripfinal.domain.member.repository.MemberRepository;
+import com.example.enjoytripfinal.domain.member.repository.RefreshTokenRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
     private final MemberRepository memberRepository;
 
-    public AuthService(MemberRepository memberRepository) {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public AuthService(MemberRepository memberRepository,RefreshTokenRepository refreshTokenRepository) {
         this.memberRepository = memberRepository;
+        this.refreshTokenRepository = refreshTokenRepository;
     }
 
     public void signUp() {
