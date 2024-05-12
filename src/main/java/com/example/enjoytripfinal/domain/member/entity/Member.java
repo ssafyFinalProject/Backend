a3e2a2acd16a38b662960ede34c4f489e2be9d21
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2024-05-12 21:12:50 +0900

    [FEAT] Member / RefreshToken Entity 추가

--- src/main/java/com/example/enjoytripfinal/domain/member/entity/Member.java
+++ src/main/java/com/example/enjoytripfinal/domain/member/entity/Member.java
@@ -1,6 +1,8 @@
 package com.example.enjoytripfinal.domain.member.entity;
 
+import com.github.f4b6a3.ulid.UlidCreator;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
@@ -8,14 +10,12 @@ import org.hibernate.annotations.GenericGenerator;
 import java.util.UUID;
 
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Member {
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)")
-    private UUID id;
+    private UUID id = UlidCreator.getMonotonicUlid().toUuid();
 
     private String email;
 
